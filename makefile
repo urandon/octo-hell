86e1ebccb25354f22d60e181bcd362fe1044b8c3
Khomutov Nikita <inikitah@gmail.com>
2012-10-16 00:36:04 +0400

    Splitter into modules

--- makefile
+++ makefile
@@ -1,1 +1,22 @@
+#
+# Makefile
+#
+SOURCES.c= main.c parser.c
+INCLUDES=parser.h
+CLAGS=
+SLIBS=
+PROGRAM= octo-hell
 
+OBJECTS= $(SOURCES.c:.c=.o)
+
+.KEEP_STATE:
+
+debug := CFLAGS= -g
+
+all debug: $(PROGRAM)
+
+$(PROGRAM): $(INCLUDES) $(OBJECTS)
+	$(LINK.c) -o $@ $(OBJECTS) $(SLIBS)
+	
+clean:
+	rm -f $(PROGRAM) $(OBJECTS)
--- parser.h
+++ parser.h
@@ -1,7 +1,22 @@
 #ifndef PARSER_H
 #define RARSER_H
 
+enum error_status{
+    EOF_ERROR       = 1,
+    BAD_QUOTES      = 2,
+    BAD_AMPERSAND   = 4,
+    BAD_CONVEYOR    = 8,
+    DUPLICATE_STDIN = 16,
+    DUPLICATE_STDOUT= 32,
+    UNKNOWN_ERROR   = 64,
+};
 
+struct exec_node{
+    char **args; /* execution arguments */
+    char *input, *output;
+    struct exec_node *next;
+};
 
+struct exec_node * parse_string(int * bg_run, int * err_status);
 
 #endif
